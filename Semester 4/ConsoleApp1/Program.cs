394a62b02188d8b543fe145d0702b765e4b9ce36
Exonault <kristiankrachmarov@gmail.com>
2022-05-11 11:12:46 +0300

    Threads examples

---
+++ Semester 4/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 69;
+            int j = 81;
+            for (int i = k; i <= j; i++)
+            {
+                string s =
+                    $@"\includegraphics[width=\textwidth,height=\textheight,keepaspectratio]{{Pics/Pic ({i}).jpg}} \\ ";
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
